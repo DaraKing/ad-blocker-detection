979f7da517de0b8421894c0106a03d121e1f1b57
DarioKnezovic <dknezovic997@gmail.com>
2023-11-11 22:31:03 +0000

    Updated ad block script to work properly.

--- src/script/detection/index.ts
+++ src/script/detection/index.ts
@@ -6,22 +6,30 @@ class Detection {
     }
 
     async check(): Promise<void> {
-
         return new Promise<void>((resolve) => {
-            const ad = document.createElement('div')
-            ad.innerHTML = '&nbsp;'
-            ad.className = 'adsbygoogle'
-            document.body.appendChild(ad)
-            window.setTimeout(() => {
-                if (ad.offsetHeight === 0) {
-                    this.adBlockEnabled = true
-                }
-                ad.remove()
+            // Create a new script element
+            const script = document.createElement('script');
+            script.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
 
-                return resolve()
-            }, 100)
-        })
+            // Set up an onerror event handler
+            script.onerror = () => {
+                this.adBlockEnabled = true; // Set the flag to true if the script fails to load
+                console.log('Adblocker detected');
+                resolve();
+            };
+
+            // Set up an onload event handler
+            script.onload = () => {
+                this.adBlockEnabled = false; // Set the flag to false if the script loads successfully
+                console.log('No adblocker detected');
+                resolve();
+            };
+
+            // Append the script to the document
+            document.head.appendChild(script);
+        });
     }
+
 }
 
 export default Detection;
